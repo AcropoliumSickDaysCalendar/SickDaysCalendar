1fce6166ccaab737656f3bb4f69b2d3a9003552a
Denis Goncharenko <deni.goncharenko@acropolium.com>
2017-09-01 14:50:31 +0300

    added firebase

---
+++ app/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase";
+
+  var config = {
+    apiKey: "AIzaSyDCqEFK6wmjzEyEIy2QhwM_hcMoG_KHw_4",
+    authDomain: "acrocalendar-ebc9b.firebaseapp.com",
+    databaseURL: "https://acrocalendar-ebc9b.firebaseio.com",
+    projectId: "acrocalendar-ebc9b",
+    storageBucket: "acrocalendar-ebc9b.appspot.com",
+    messagingSenderId: "204469278632"
+  };
+  firebase.initializeApp(config);
